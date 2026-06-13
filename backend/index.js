727a6d661d33d665922daa49f46b3bfac4b49af1
Aman Kalmodiya <amankalmodiya7898@gmail.com>
2026-06-13 11:09:15 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ const app = express();
 const port = process.env.PORT;
 app.use(cors({
     origin: ["http://localhost:5173", "https://ai-virtual-assistant-b4m2.onrender.com"],
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"]
 }));
 
 app.use(express.json())
