6f7564e943d74967abd89a9de1937b35ef9366ca
Aman Kalmodiya <amankalmodiya7898@gmail.com>
2026-06-13 10:18:45 +0530

    cors fix for production

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,10 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-        credentials:true
-    }
-));
+app.use(cors({
+    origin: ["http://localhost:5173", "https://ai-virtual-assistant-b4m2.onrender.com"],
+    credentials: true
+}));
 
 app.use(express.json())
 app.use(cookieParser()) 
